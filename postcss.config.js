5bd31d7c9014d11011b574fb6c4a097563a0e45a
subuta <shunta.3110@gmail.com>
2019-06-08 10:35:51 +0900

    Add initial files.

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: [
+    require('tailwindcss'),
+    require('postcss-import'),
+    require('autoprefixer'),
+    require('precss')
+  ]
+}
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  theme: {
+    extend: {},
+    fontFamily: {}
+  },
+  variants: {},
+  plugins: []
+}
